406b9ea89e6d05ec2d5c0b4a67e05c5d4c737c23
jamie-jungah <wjddk718@gmail.com>
2023-12-01 01:45:32 +0900

    feat: react-router-dom 컴포넌트화

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import './App.css';
-
-import { Route, Routes, Navigate } from 'react-router-dom';
+import Router from 'components/Router';
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<h1>Home Page</h1>} />
-      <Route path='/posts' element={<h1>Post List</h1>} />
-      <Route path='/posts/:id' element={<h1>Post Detail</h1>} />
-      <Route path='/posts/new' element={<h1>New Post</h1>} />
-      <Route path='/posts/edit/:id' element={<h1>Edit Post</h1>} />
-      <Route path='/profile' element={<h1>Profile Page</h1>} />
-      <Route path='/profile/edit' element={<h1>Edit Profile Page</h1>} />
-      <Route path='/notifications' element={<h1>Notification Page</h1>} />
-      <Route path='/search' element={<h1>Search Page</h1>} />
-      <Route path='/users/login' element={<h1>Login Page</h1>} />
-      <Route path='/users/signup' element={<h1>Signup Page</h1>} />
-      <Route path='*' element={<Navigate replace to='/' />} />
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/components/Router.tsx
@@ -0,0 +1,31 @@
+import HomePage from 'pages/home';
+import NotificationsPage from 'pages/notifications';
+import PostListPage from 'pages/posts';
+import PostDetail from 'pages/posts/detail';
+import EditPost from 'pages/posts/edit';
+import NewPost from 'pages/posts/new';
+import ProfilePage from 'pages/profile';
+import EditProfile from 'pages/profile/edit';
+import SearchPage from 'pages/search';
+import LoginPage from 'pages/users/login';
+import SignupPage from 'pages/users/signup';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/posts' element={<PostListPage />} />
+      <Route path='/posts/:id' element={<PostDetail />} />
+      <Route path='/posts/new' element={<NewPost />} />
+      <Route path='/posts/edit/:id' element={<EditPost />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='/profile/edit' element={<EditProfile />} />
+      <Route path='/notifications' element={<NotificationsPage />} />
+      <Route path='/search' element={<SearchPage />} />
+      <Route path='/users/login' element={<LoginPage />} />
+      <Route path='/users/signup' element={<SignupPage />} />
+      <Route path='*' element={<Navigate replace to='/' />} />
+    </Routes>
+  );
+}
