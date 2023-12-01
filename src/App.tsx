9de7d32c26c5ef8f0357cdc9a0cc40f2a90116b5
jamie-jungah <wjddk718@gmail.com>
2023-12-01 21:16:53 +0900

    feat: Firebase Auth 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 import { Layout } from 'components/Layout';
 import Router from 'components/Router';
 
+import { getAuth } from 'firebase/auth';
+import { app } from 'firebaseApp';
+
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <Layout>
       <Router />
