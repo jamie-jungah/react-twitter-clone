6128820802b32ae6737501814db6eb86730dc05c
jamie-jungah <wjddk718@gmail.com>
2023-12-02 00:50:01 +0900

    feat: Firebase Auth 회원가입

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Layout } from 'components/Layout';
 import Router from 'components/Router';
 
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { app } from 'firebaseApp';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import Loader from 'components/loader/Loader';
 
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Layout>
-      <Router />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
