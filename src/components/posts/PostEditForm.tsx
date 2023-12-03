e739f1310f67abad3615cfb00f903711675b9ddf
jamie-jungah <wjddk718@gmail.com>
2023-12-04 07:43:14 +0900

    feat: 해시태그 CRUD

--- src/components/posts/PostEditForm.tsx
+++ src/components/posts/PostEditForm.tsx
@@ -8,9 +8,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { PostProps } from 'pages/home';
 
 export default function PostEditForm() {
-  const params = useParams();
   const [content, setContent] = useState<string>('');
   const [post, setPost] = useState<PostProps | null>(null);
+  const [hashtag, setHastag] = useState<string>('');
+  const [hashtags, setHashtags] = useState<string[]>([]);
+  const params = useParams();
   const navigate = useNavigate();
 
   const handleFileUpload = () => {};
@@ -21,6 +23,7 @@ export default function PostEditForm() {
       const docSnap = await getDoc(docRef);
       setPost({ ...(docSnap?.data() as PostProps), id: docSnap.id });
       setContent(docSnap?.data()?.content);
+      setHashtags(docSnap?.data()?.hashtags);
     }
   }, [params.id]);
 
@@ -32,6 +35,7 @@ export default function PostEditForm() {
         const postRef = doc(db, 'posts', post?.id);
         await updateDoc(postRef, {
           content: content,
+          hashtags: hashtags,
         });
         toast.success('게시글을 수정했습니다.');
         navigate(`posts/${post?.id}`);
@@ -48,6 +52,28 @@ export default function PostEditForm() {
 
     if (name === 'content') {
       setContent(value);
+    }
+  };
+
+  const removeHashtag = (tag: string) => {
+    setHashtags(hashtags?.filter((value) => value !== tag));
+  };
+
+  const onChangeHashtag = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHastag(e?.target?.value?.trim());
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === ' ' && e.currentTarget.value.trim() !== '') {
+      // 같은 태그가 있으면 에러 띄우기 아니라면 생성
+      if (hashtags?.includes(e.currentTarget.value.trim())) {
+        toast.error('이미 입력한 태그가 있습니다.');
+      } else {
+        setHashtags((prev) =>
+          prev?.length > 0 ? [...prev, hashtag] : [hashtag]
+        );
+        setHastag('');
+      }
     }
   };
 
@@ -68,6 +94,28 @@ export default function PostEditForm() {
         onChange={onChange}
         value={content}
       />
+      <div className='post-form__hashtags'>
+        <span className='post-form__hastags-outputs'>
+          {hashtags?.map((tag, index) => (
+            <span
+              className='post-form__hashtags-tag'
+              key={index}
+              onClick={() => removeHashtag(tag)}
+            >
+              #{tag}
+            </span>
+          ))}
+        </span>
+        <input
+          className='post-form__input'
+          name='hashtag'
+          id='hashtag'
+          placeholder='해시태그 + 스페이스 바 입력'
+          onChange={onChangeHashtag}
+          onKeyUp={handleKeyUp}
+          value={hashtag}
+        />
+      </div>
       <div className='post-form__submit-area'>
         <label htmlFor='file-input' className='post-form__file'>
           <FiImage className='post-form__file-icon' />
--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -7,6 +7,8 @@ import AuthContext from 'context/AuthContext';
 
 export default function PostForm() {
   const [content, setContent] = useState<string>('');
+  const [hashtag, setHastag] = useState<string>('');
+  const [hashtags, setHashtags] = useState<string[]>([]);
   const { user } = useContext(AuthContext);
 
   const handleFileUpload = () => {};
@@ -24,8 +26,11 @@ export default function PostForm() {
         }),
         uid: user?.uid,
         email: user?.email,
+        hashtags: hashtags,
       });
       setContent('');
+      setHashtags([]);
+      setHastag('');
       toast.success('게시글을 생성했습니다.');
     } catch (e: any) {
       console.log(e);
@@ -42,6 +47,28 @@ export default function PostForm() {
     }
   };
 
+  const removeHashtag = (tag: string) => {
+    setHashtags(hashtags?.filter((value) => value !== tag));
+  };
+
+  const onChangeHashtag = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHastag(e?.target?.value?.trim());
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === ' ' && e.currentTarget.value.trim() !== '') {
+      // 같은 태그가 있으면 에러 띄우기 아니라면 생성
+      if (hashtags?.includes(e.currentTarget.value.trim())) {
+        toast.error('이미 입력한 태그가 있습니다.');
+      } else {
+        setHashtags((prev) =>
+          prev?.length > 0 ? [...prev, hashtag] : [hashtag]
+        );
+        setHastag('');
+      }
+    }
+  };
+
   return (
     <form className='post-form' onSubmit={onSubmit}>
       <textarea
@@ -53,6 +80,28 @@ export default function PostForm() {
         onChange={onChange}
         value={content}
       />
+      <div className='post-form__hashtags'>
+        <span className='post-form__hastags-outputs'>
+          {hashtags?.map((tag, index) => (
+            <span
+              className='post-form__hashtags-tag'
+              key={index}
+              onClick={() => removeHashtag(tag)}
+            >
+              #{tag}
+            </span>
+          ))}
+        </span>
+        <input
+          className='post-form__input'
+          name='hashtag'
+          id='hashtag'
+          placeholder='해시태그 + 스페이스 바 입력'
+          onChange={onChangeHashtag}
+          onKeyUp={handleKeyUp}
+          value={hashtag}
+        />
+      </div>
       <div className='post-form__submit-area'>
         <label htmlFor='file-input' className='post-form__file'>
           <FiImage className='post-form__file-icon' />
